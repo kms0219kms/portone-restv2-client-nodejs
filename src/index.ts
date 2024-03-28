f2f862c6903d2b5894544f6ae393f4dd483e19c8
Minsu Kim <minsu_kim@bishanoi.net>
2024-03-28 03:10:13 +0000

    :sparkles: feat: PortOne SDK 초기 구성 및 Auth/Payments 스펙 배포

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,73 @@
-console.log("Try npm run lint/fix!");
+import {DEFAULT_HOST} from './common/constants';
+import createRequestInstance from './common/request';
 
-const longString = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ut aliquet diam.';
+import {auth} from './lib/auth';
+import {payments} from './lib/payments';
 
-const trailing = 'Semicolon'
+/**
+ * PortOne RestAPI v2 SDK Class.
+ *
+ * @param {object} authorization - Object that includes authorization data.
+ * @param {string} authorization.type - Type of authorization.
+ * @param {string} authorization.secret - Secret key or access token.
+ * @param {string} host - Host URL.
+ *
+ * @default authorization.type = "SECRET"
+ * @default host = "https://api.portone.io"
+ *
+ * @example
+ * const portOne = new PortOne({
+ *    authorization: {
+ *       type: "SECRET",
+ *       secret: "MY_API_SECRET"
+ *    }
+ * })
+ */
+export class PortOne {
+  private _request = createRequestInstance(
+    this.host,
+    `PortOne ${this.authorization.secret}`
+  );
 
-			const why={am:'I tabbed?'};
+  constructor(
+    public host: string,
+    public authorization: {
+      type: 'SECRET' | 'ACCESS_TOKEN';
+      secret: string;
+    },
+    public storeId?: string
+  ) {
+    /**
+     * Set default values.
+     */
+    this.authorization.type = this.authorization.type || 'SECRET';
+    this.host = this.host || DEFAULT_HOST;
+    this._init();
+  }
 
-const iWish = "I didn't have a trailing space..."; 
+  private async _init() {
+    if (this.authorization.type === 'ACCESS_TOKEN') {
+      const token = await this.auth.token({
+        apiSecret: this.authorization.secret,
+      });
 
-const sicilian = true;;
+      if (!token.data?.accessToken || token.error)
+        throw new Error(
+          token.error
+            ? `${token.error?.type}: ${token.error?.message}`
+            : 'Failed to get access token'
+        );
 
-const vizzini = (!!sicilian) ? !!!sicilian : sicilian;
+      this.authorization.secret = token.data.accessToken;
+      this._request = createRequestInstance(
+        this.host,
+        `Bearer ${this.authorization.secret}`
+      );
+    }
+  }
 
-const re = /foo   bar/;
-
-export function doSomeStuff(withThis: string, andThat: string, andThose: string[]) {
-    //function on one line
-    if(!Boolean(andThose.length)) {return false;}
-    console.log(withThis);
-    console.log(andThat);
-    console.dir(andThose);
-    console.log(longString, trailing, why, iWish, vizzini, re);
-    return;
+  public readonly auth = auth(this._request);
+  public readonly payments = payments(this._request, {
+    storeId: this.storeId || undefined,
+  });
 }
-// TODO: more examples
